eddf02c282fe049b189e17c729de70d8b716cdcd
Fpf1 <danielcontrerasescalante73@gmail.com>
2026-05-28 13:12:44 -0500

    update

--- fletes-black/src/views/dashboard/DashboardView.vue
+++ fletes-black/src/views/dashboard/DashboardView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="p-8">
     <h2 class="text-2xl font-bold text-gray-900 mb-2">
-      Bienvenido, {{ auth.user?.firstName }} 👋
+      Bienvenidoooooo, {{ auth.user?.firstName }} 👋
     </h2>
-    <p class="text-gray-500">Panel de control de Fletes Black</p>
+    <p class="text-gray-500">Panel de control de Fletes Blackkkkkkkkkkk</p>
   </div>
 </template>
 
